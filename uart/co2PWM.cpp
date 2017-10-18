d5f9a3718cb1cf017b29a20c501589bc40e738fd
wasilewski94 <wasilkris@gmail.com>
2017-10-18 16:11:40 +0000

    timestamp do czujnika CO2 (PWM)

--- uart/co2PWM.cpp
+++ uart/co2PWM.cpp
@@ -1,11 +1,44 @@
-//program: co2PWM
-//compile with:    g++ co2PWM.cpp -o co2PWM  -lwiringPi
-//run with:        sudo ./co2UART
 #include <iostream>
+#include <stdio.h>
+#include <fstream>
+#include <iomanip>
+#include <ctime>
+#include <chrono>
+#include <sys/time.h>
+#include <unistd.h>
+#include <cstring>
+#include <cerrno>
+#include <system_error>
+#include <string>
 #include <wiringPi.h>
+#include <csignal>
+
+#define _BSD_SOURCE
+
 using namespace std;
 
 const int PWM=2;
+bool time_to_quit=false;
+
+void sig_handler(int signo)
+{
+  cout << "It's time to quit.\n";
+  time_to_quit=true;
+}
+
+void init()
+{
+  wiringPiSetup();
+  pinMode(PWM, INPUT);
+  cout << " CO2 PWM program is running .. \n"; 
+}
+
+string GetTimeStamp() {
+    struct timeval tv;
+    gettimeofday(&tv,NULL);
+    string s = to_string(tv.tv_sec) + ";" + to_string(tv.tv_usec);
+    return s;
+}
 
 int getPWM()
 {
@@ -26,15 +59,32 @@ int getPWM()
         long co2ppm = 5 * ((duration/1000) - 2);
 	return co2ppm;
 }
+
+
 int main(void)
 {
-        wiringPiSetup();
-        pinMode(PWM, INPUT);
-       	cout << " CO2 PWM program is running .. \n";
-	while (1)
+	if (signal(SIGINT, sig_handler) == SIG_ERR)
+	    printf("can't catch SIGINT\n");
+	init();
+	fstream plik;
+	//string utime;
+	
+	plik.open( "pomiarCO2.txt", ios::in | ios::out );
+	if( plik.good() == true )
 	{
-               cout << "\nco2 ppm = \t"<< getPWM();
-               delay (5000);   //wait  30 sekunds
+	    cout << "Otwieranie pliku zakonczone sukcesem!" << endl;
+	} else {
+	    cout << "Nie udalo sie otworzyc pliku!" << endl;
+	    cerr << "Error: " << strerror(errno) << endl;
 	}
+	while(!time_to_quit)
+	{
+	 string s = to_string(getPWM());
+	 s += ";" + GetTimeStamp(); 
+	 cout << s << endl;
+	 plik << s << '\n';
+	   sleep(1);
+	}
+	plik.close();
 	return 0;
 }
